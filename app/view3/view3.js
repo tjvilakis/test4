3c298c2990e5081e5088814c0aa3a8a933d8a838
Castor <tomvilakis3612@gmail.com>
2016-08-27 11:37:35 -0700

    button

--- app/view3/view3.js
+++ app/view3/view3.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('myApp.view1', ['ngRoute'])
+angular.module('myApp.view3', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view3', {
