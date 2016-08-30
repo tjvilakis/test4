05816af4aa8e676ab8921e42cc3a5058beb1b553
Castor <tomvilakis3612@gmail.com>
2016-08-29 22:02:34 -0700

    button

--- app/testmong.js
+++ app/testmong.js
@@ -19,7 +19,21 @@ MongoClient.connect(url, function (err, db) {
         console.log('Connection established to', url);
 
         // do some work here with the database.
+// Get the documents collection
+        var collection = db.collection('users');
 
+        //Create some users
+        var user1 = {name: 'modulus admin', age: 42, roles: ['admin', 'moderator', 'user']};
+        var user2 = {name: 'modulus user', age: 22, roles: ['user']};
+        var user3 = {name: 'modulus super admin', age: 92, roles: ['super-admin', 'admin', 'moderator', 'user']};
+
+        // Insert some users
+        collection.insert([user1, user2, user3], function (err, result) {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log('Inserted %d documents into the "users" collection. The documents inserted with "_id" are:', result.length, result);
+            }
         //Close connection
         db.close();
     }
