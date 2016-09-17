329db6e69e2e7a98127e48e5b8fae380b6ff4d54
Castor <tomvilakis3612@gmail.com>
2016-09-17 10:44:28 -0700

    k

--- app/view1/top100.js
+++ app/view1/top100.js
@@ -1,14 +1,7 @@
-/**
- * Created by Castor on 9/17/2016.
- */
-var app = angular.module('myApp', []);
+// Code goes here
 
-app.service('hexafy', function() {
-    this.myFunc = function (x) {
-        return x.toString(16);
-    }
-});
-app.controller('myCtrl', function($scope, hexafy) {
-    $scope['hex'] = hexafy.myFunc(255);
-    $scope.$apply;
-});
+angular.module('app', []).controller('MainController', MainController);
+
+function MainController($scope) {
+    $scope.angularMessage = "Test test test test"
+}
