84d607f343a1eb0d7f24435a11b402ea28be7aa8
Castor <tomvilakis3612@gmail.com>
2016-09-17 12:57:41 -0700

    k

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -10,14 +10,6 @@ angular.module('myApp.view1', ['ngRoute'])
 }])
 
 .controller('View1Ctrl', ['$scope', function($scope) {
-    $scope.test = "angular sucks ballsacks";
-
-  app.service('hexafy', function() {
-    this.myFunc = function (x) {
-      return x.toString(16);
-    }
-  });
-
-  $scope.hex = hexafy.myFunc(255);
+  $scope.test = "angular sucks ballsacks";
 
 }]);
