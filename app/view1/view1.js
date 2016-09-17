6dcd9bea17ee354ed58d0ca93999b68ad7f3b7d3
Castor <tomvilakis3612@gmail.com>
2016-09-17 13:13:27 -0700

    k

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -12,9 +12,13 @@ angular.module('myApp.view1', ['ngRoute'])
 .controller('View1Ctrl', ['$scope', function($scope, $http) {
   //$scope.test = "angular sucks ballsacks";
 
-  $http.get('http://52.53.202.129:3000/top100')
-      .then(function(response) {
-        $scope.test = response.data;
+
+  $http.get('http://52.53.202.129:3000/top100').
+  success(function(data, status, headers, config) {
+    $scope.entconn2 = data;
+  }).
+  error(function(data, status, headers, config) {
+    // log error
   });
 
 
