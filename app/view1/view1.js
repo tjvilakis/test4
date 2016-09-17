1624a5818d60f5acb2f0f3dd21b68f17084a23e6
Castor <tomvilakis3612@gmail.com>
2016-09-17 13:50:05 -0700

    k

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -12,10 +12,10 @@ angular.module('myApp.view1', ['ngRoute'])
 .controller('View1Ctrl', ['$scope', '$http', function($scope, $http) {
   //$scope.test = "test";
   //the above works when I bind in view1.html
-
+  $scope.information = {};
   $http.get('http://52.53.202.129:3000/top100').
   success(function(response) {
-    $scope.information = response;
+    $scope.information = response.data.components;
   });
 
 }]);
