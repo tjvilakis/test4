ae4f960ac1edada3bb3100d7a35a6cca7232044d
Castor <tomvilakis3612@gmail.com>
2016-09-17 13:30:12 -0700

    k

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -9,14 +9,13 @@ angular.module('myApp.view1', ['ngRoute'])
   });
 }])
 
-
 .controller('View1Ctrl', ['$scope', '$http', function($scope, $http) {
   //$scope.test = "test";
   //the above works when I bind in view1.html
 
   $http.get('http://52.53.202.129:3000/top100').
   success(function(response) {
-    $scope.dateCollected = response;
+    $scope.information = response;
   });
 
 }]);
