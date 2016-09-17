5809903d43f6c6024f36e8f301a3fd796053918f
Castor <tomvilakis3612@gmail.com>
2016-09-17 12:14:01 -0700

    k

--- app/app.js
+++ app/app.js
@@ -6,8 +6,7 @@ angular.module('myApp', [
   'myApp.view1',
   'myApp.view2',
   'myApp.view3',
-  'myApp.version',
-  'demoApp'
+  'myApp.version'
 ]).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
   $locationProvider.hashPrefix('!');
