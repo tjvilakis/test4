a6852bc1fd0964924eab5042c48aefd7d67aba73
Castor <tomvilakis3612@gmail.com>
2016-09-17 13:16:36 -0700

    k

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -9,18 +9,18 @@ angular.module('myApp.view1', ['ngRoute'])
   });
 }])
 
-.controller('View1Ctrl', ['$scope', function($scope, $http) {
-  //$scope.test = "angular sucks ballsacks";
 
+.controller('View1Ctrl', ['$scope', '$http', function($scope, $http) {
+  //$scope.test = "test";
+  //the above works when I bind in view1.html
 
+//from http://fdietz.github.io/recipes-with-angular-js/consuming-external-services/requesting-json-data-with-ajax.html
   $http.get('http://52.53.202.129:3000/top100').
   success(function(data, status, headers, config) {
-    $scope.entconn2 = data;
+    $scope.info = data;
   }).
   error(function(data, status, headers, config) {
     // log error
   });
-
-
 
 }]);
