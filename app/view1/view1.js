3096e74ddcf127f2b6af3f25fdbb85c9ea1d57e9
Castor <tomvilakis3612@gmail.com>
2016-09-17 13:06:42 -0700

    k

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -10,13 +10,13 @@ angular.module('myApp.view1', ['ngRoute'])
 }])
 
 .controller('View1Ctrl', ['$scope', function($scope, $http) {
-  $scope.test = "angular sucks ballsacks";
-/*
+  //$scope.test = "angular sucks ballsacks";
+
   $http.get('http://52.53.202.129:3000/top100')
       .then(function(response) {
         $scope.test = response.data;
   });
-  */
+
 
 
 }]);
