5476e3dc21ec054011d1f13ad63990038768a8df
Castor <tomvilakis3612@gmail.com>
2016-09-17 14:04:29 -0700

    k

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -2,39 +2,23 @@
 
 angular.module('myApp.view1', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {
-    templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl',
-      factory: 'jsonFactory'
-  });
-}])
+    .config(['$routeProvider', function($routeProvider) {
+        $routeProvider.when('/view1', {
+            templateUrl: 'view1/view1.html',
+            controller: 'View1Ctrl'
+        });
+    }])
 
-.factory('jsonFactory', function ($q, $http) {
-  return {
-    getOtherStuff: function () {
-      var deferred = $q.defer(),
-          httpPromise = $http.get('http://52.53.202.129:3000/top100');
+    .controller('View1Ctrl', ['$scope', '$http', function($scope, $http) {
+        //$scope.test = "test";
+        //the above works when I bind in view1.html
 
-      httpPromise.then(function (response) {
-        deferred.resolve(response);
-      }, function (error) {
-        console.error(error);
-      });
+        $http.get('http://52.53.202.129:3000/top100')
+            .then(function (response) {
+                // Success
+                angular.copy(response.data, $scope.information);
+            }, function () {
+                // Failure
+            });
 
-      return deferred.promise;
-    }
-  };
-})
-
-.controller('View1Ctrl', ['$scope', '$http', function($scope, $http) {
-  //$scope.test = "test";
-  //the above works when I bind in view1.html
-  $scope.otherStuff = {};
-  jsonFactory.getOtherStuff()
-      .then(function (response) {
-        $scope.otherStuff = response.data.components;
-      }, function (error) {
-        console.error(error);
-      });
-}]);
+    }]);
