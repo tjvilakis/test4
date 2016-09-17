9527a625e077115ebd6489cbfb2d1a92e6f75ad4
Castor <tomvilakis3612@gmail.com>
2016-09-17 14:36:52 -0700

    I

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -11,8 +11,10 @@ angular.module('myApp.view1', ['ngRoute'])
 
     .controller('View1Ctrl', ['$scope', '$http', function($scope, $http) {
         //$scope.test = "test";
+
+        $scope.information = {};
         //the above works when I bind in view1.html
-/*
+
         $http.get('http://52.53.202.129:3000/top100')
             .then(function (response) {
                 // Success
@@ -21,18 +23,19 @@ angular.module('myApp.view1', ['ngRoute'])
             }, function () {
                 //console.error(error);
             });
-            */
-        $scope.information = {};
+        /*
         $http({
             method: 'GET',
             url: 'http://52.53.202.129:3000/top100l'
         }).then(function successCallback(response) {
             // this callback will be called asynchronously
             // when the response is available
-            angular.copy(response.data, $scope.information);
+            //angular.copy(response.data, $scope.information);
+
         }, function errorCallback(response) {
             // called asynchronously if an error occurs
             // or server returns response with an error status.
         });
+        */
 
     }]);
