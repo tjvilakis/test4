ccd71342f0cc2be631ff430b589af18aa45ff13c
Castor <tomvilakis3612@gmail.com>
2016-09-17 13:53:17 -0700

    k

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -9,13 +9,32 @@ angular.module('myApp.view1', ['ngRoute'])
   });
 }])
 
+    .factory('jsonFactory', function ($q, $http) {
+      return {
+        getOtherStuff: function () {
+          var deferred = $q.defer(),
+              httpPromise = $http.get('http://52.53.202.129:3000/top100');
+
+          httpPromise.then(function (response) {
+            deferred.resolve(response);
+          }, function (error) {
+            console.error(error);
+          });
+
+          return deferred.promise;
+        }
+      };
+    })
+
 .controller('View1Ctrl', ['$scope', '$http', function($scope, $http) {
   //$scope.test = "test";
   //the above works when I bind in view1.html
-  $scope.information = {};
-  $http.get('http://52.53.202.129:3000/top100').
-  success(function(response) {
-    $scope.information = response.data.components;
-  });
+  $scope.otherStuff = {};
+  jsonFactory.getOtherStuff()
+      .then(function (response) {
+        $scope.otherStuff = response.data.components;
+      }, function (error) {
+        console.error(error);
+      });
 
 }]);
