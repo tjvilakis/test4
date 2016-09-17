8ce2b26a6b52d043f3f874e497548cd5205cbc3f
Castor <tomvilakis3612@gmail.com>
2016-09-17 10:04:03 -0700

    k

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -10,11 +10,13 @@ angular.module('myApp.view1', ['ngRoute'])
 }])
 
 .controller('View1Ctrl', [function() {
-  var app = angular.module('myApp', []);
-  app.controller('myCtrl', function($scope, $http) {
-    $http.get("http://52.53.202.129:3000/top100")
-        .then(function(response) {
-          $scope.top100 = response.data;
-        });
-  });
+
 }]);
+
+var app = angular.module('myApp', []);
+app.controller('myCtrl', function($scope, $http) {
+  $http.get("http://52.53.202.129:3000/top100")
+      .then(function(response) {
+        $scope.top100 = response.data;
+      });
+});
