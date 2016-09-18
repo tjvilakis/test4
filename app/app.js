262279c2f0e3a9d3e3433c8ec25a9b37ef4f1a24
Castor <tomvilakis3612@gmail.com>
2016-09-17 19:58:33 -0700

    I

--- app/app.js
+++ app/app.js
@@ -6,8 +6,7 @@ angular.module('myApp', [
   'myApp.view1',
   'myApp.view3',
   'myApp.version',
-  'Landing',
-  'datviewer'
+  'Landing'
 
 ]).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
